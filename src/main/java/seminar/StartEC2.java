40a2ec25e1f9ab9a0e24376fbcbc6de094d52140
Andreas Tscheinig <andreas.tscheinig@dynatrace.com>
2020-04-20 12:48:20 +0200

    Ensure key-pair

--- src/main/java/seminar/StartEC2.java
+++ src/main/java/seminar/StartEC2.java
@@ -1,5 +1,12 @@
 package seminar;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -12,17 +19,24 @@ import javax.annotation.Nonnull;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.AuthorizeSecurityGroupIngressRequest;
 import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.CreateKeyPairRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairResult;
 import com.amazonaws.services.ec2.model.CreateSecurityGroupRequest;
 import com.amazonaws.services.ec2.model.CreateSecurityGroupResult;
 import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
 import com.amazonaws.services.ec2.model.DescribeImagesRequest;
 import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.DescribeKeyPairsRequest;
+import com.amazonaws.services.ec2.model.DescribeKeyPairsResult;
 import com.amazonaws.services.ec2.model.DescribeSecurityGroupsResult;
 import com.amazonaws.services.ec2.model.Filter;
 import com.amazonaws.services.ec2.model.Image;
 import com.amazonaws.services.ec2.model.IpPermission;
 import com.amazonaws.services.ec2.model.IpRange;
+import com.amazonaws.services.ec2.model.KeyPair;
+import com.amazonaws.services.ec2.model.KeyPairInfo;
 import com.amazonaws.services.ec2.model.SecurityGroup;
 
 public class StartEC2 {
@@ -31,6 +45,7 @@ public class StartEC2 {
 
 	private static final String IMAGE_ID_UBUNTU = "ami-07ebfd5b3428b6f4d";
 	private static final String SECURITY_GROUP_NAME = "assignment4";
+	private static final String KEY_PAIR_NAME = "assignment4";
 
 	public static void main(String[] args) {
 
@@ -47,7 +62,9 @@ public class StartEC2 {
 				createIpPermission(22),
 				createIpPermission(80)
 		);
+		authorizeSecurityGroupIngressRequest(ec2Client, securityGroupId, permissions);
 
+		ensureKeyPair(ec2Client);
 
 	}
 
@@ -68,7 +85,7 @@ public class StartEC2 {
 	 * @param client Amazon EC2 client
 	 */
 	private static void listAllAvailabilityZones(@Nonnull final AmazonEC2 client) {
-		final DescribeAvailabilityZonesResult result = client.describeAvailabilityZones();;
+		final DescribeAvailabilityZonesResult result = client.describeAvailabilityZones();
 
 		for (final AvailabilityZone zone : result.getAvailabilityZones()) {
 			final String message = "Zone " +
@@ -149,5 +166,81 @@ public class StartEC2 {
 				.withIpProtocol("tcp")
 				.withFromPort(port)
 				.withToPort(port);
+	}
+
+	private static void authorizeSecurityGroupIngressRequest(
+			@Nonnull final AmazonEC2 client,
+			@Nonnull final String securityGroupId,
+			@Nonnull final List<IpPermission> ipPermissions
+	) {
+		final AuthorizeSecurityGroupIngressRequest request = new AuthorizeSecurityGroupIngressRequest()
+				.withGroupId(securityGroupId)
+				.withIpPermissions(ipPermissions);
+
+		try {
+			client.authorizeSecurityGroupIngress(request);
+
+			logger.log(Level.INFO, "Security rule created");
+		} catch (final Exception e) {
+			logger.log(Level.INFO, "Security rule already exists", e.fillInStackTrace());
+		}
+	}
+
+	/**
+	 * Get or create a new key-pair
+	 * @param client Amazon EC2 client
+	 * @return Key-pair name
+	 */
+	@Nonnull
+	private static String ensureKeyPair(@Nonnull final AmazonEC2 client) {
+		final DescribeKeyPairsRequest request = new DescribeKeyPairsRequest()
+				.withKeyNames(KEY_PAIR_NAME);
+
+		final DescribeKeyPairsResult result = client.describeKeyPairs(request);
+
+		if (!result.getKeyPairs().isEmpty()) {
+			final KeyPairInfo keyPairInfo = result.getKeyPairs().iterator().next();
+			logger.log(Level.INFO, "Existing key-pair found {0}", keyPairInfo);
+
+			return keyPairInfo.getKeyName();
+		}
+
+		final CreateKeyPairRequest createRequest = new CreateKeyPairRequest()
+				.withKeyName(KEY_PAIR_NAME);
+		final CreateKeyPairResult createResult = client.createKeyPair(createRequest);
+		final KeyPair keyPair =  createResult.getKeyPair();
+		logger.log(Level.INFO, "Created a new key-pair {0}", keyPair);
+
+		writeKeyPairToFileSystem(keyPair);
+
+		return keyPair.getKeyName();
+	}
+
+
+	/**
+	 * Writes given {@code keyPair} to user's home directory Documents folder.
+	 * @param keyPair Key-pair
+	 */
+	private static void writeKeyPairToFileSystem(@Nonnull final KeyPair keyPair) {
+		final String path = System.getProperty("user.home") + "/Documents";
+		final File file = new File(path);
+
+		final Reader reader = new BufferedReader(new StringReader(keyPair.getKeyMaterial()));
+		try (final Writer writer = new BufferedWriter(new FileWriter(file))) {
+			final char[] buffer = new char[1024];
+			int length;
+
+			while ((length = reader.read(buffer)) != -1) {
+				writer.write(buffer,0 , length);
+			}
+
+			writer.flush();
+			reader.close();
+			reader.close();
+
+			logger.log(Level.INFO, "Written key-pair material to file {0}", path);
+		} catch (final Exception e) {
+			logger.log(Level.INFO, "Could not write key-pair material to file {0}", path);
+		}
 	}
 }
